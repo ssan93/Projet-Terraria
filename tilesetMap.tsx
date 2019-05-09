c1a6e82bb4257dc2b68169e0d8b0431fbc99f15d
Steven <san.steven97@gmail.com>
2019-05-09 21:00:17 +0200

    je vais le remettre dans un dossier

--- tilesetMap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="tilesetMap" tilewidth="32" tileheight="32" tilecount="450" columns="25">
- <image source="tilesetMap.png" width="800" height="600"/>
-</tileset>
