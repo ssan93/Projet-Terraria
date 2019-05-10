727b374ca12c819633329992c8d131be14864e8f
crunchitaip <san.steven97@gmail.com>
2019-05-10 15:26:17 +0200

    map

--- src/modele/Tuiles.java
+++ src/modele/Tuiles.java
@@ -5,7 +5,7 @@ import javafx.scene.image.Image;
 public class Tuiles {
 
 	public static Image selectionTuile(int i) {
-		return new Image("src/vue.tiles/tile"+i+".png");
+		return new Image("vue/tiles/tile"+i+".png");
 		
 	}
 	
