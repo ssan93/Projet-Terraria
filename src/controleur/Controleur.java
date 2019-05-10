d727eea0877d4240101c59763f2ec74ffec9616b
crunchitaip <san.steven97@gmail.com>
2019-05-10 14:41:37 +0200

    Map complete

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -16,7 +16,7 @@ import modele.Tuiles;
 public class Controleur implements Initializable {
 	@FXML
 	private Pane sol;
-	Carte tileSol = new Carte("src/carte.txt", "src/carte2.txt");	
+	Carte tileSol = new Carte("src/carte.txt", "src/carte2.txt", "src/carte3.txt");	
 	int temps;
 	private Timeline loop;
 
@@ -29,12 +29,32 @@ public class Controleur implements Initializable {
 	}
 
 	public void creerVue(Carte map, Pane lePane) {
+		int t2[][] = map.getMapBg();
+		for (int x = 0; x < t2.length; x++) {
+			for (int y = 0; y < t2[x].length; y++) {
+				if (t2[x][y] != 0) {
+				ImageView img = new ImageView(Tuiles.selectionTuile(t2[x][y]));
+				img.relocate(y * 32, x * 32);
+				sol.getChildren().add(img);}
+			}
+		}
+		
+		int t1[][] = map.getMapMilieu();
+		for (int x = 0; x < t1.length; x++) {
+			for (int y = 0; y < t1[x].length; y++) {
+				if (t1[x][y] != 0) {
+				ImageView img = new ImageView(Tuiles.selectionTuile(t1[x][y]));
+				img.relocate(y * 32, x * 32);
+				sol.getChildren().add(img);}
+			}
+		}
 		int t[][] = map.getMapSol();
 		for (int x = 0; x < t.length; x++) {
 			for (int y = 0; y < t[x].length; y++) {
+				if (t[x][y] != 0) {
 				ImageView img = new ImageView(Tuiles.selectionTuile(t[x][y]));
-				img.relocate(y * 48, x * 48);
-				sol.getChildren().add(img);
+				img.relocate(y * 32, x * 32);
+				sol.getChildren().add(img);}
 			}
 		}
 	}
@@ -47,7 +67,7 @@ public class Controleur implements Initializable {
 		KeyFrame kf = new KeyFrame(
 			Duration.seconds(0.033), (ev ->{
 				if(temps%30==0) {
-					sol.relocate(sol.getLayoutX()-(48*5),sol.getLayoutY()-(48*5));
+					//sol.relocate(sol.getLayoutX()-(48*5),sol.getLayoutY()-(48*5));
 					System.out.println("tac");
 				}
 				temps++;
