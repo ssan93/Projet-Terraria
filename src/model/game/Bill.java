f4f4e7ea873fbfcd75113f8dd0c8e904348753d1
romain <romain@192.168.48.213>
2019-06-05 10:48:08 +0200

    zef

--- src/model/game/Bill.java
+++
@@ -1,8 +0,0 @@
-package model.game;
-
-public class Bill extends Character{
-
-	public Bill(int x, int y, int vitesse) {
-		super(x, y, vitesse, 100);
-	}
-}
--- src/model/game/StaticObject.java
+++
@@ -1,9 +0,0 @@
-package model.game;
-
-public class StaticObject extends Objet{
-
-	public StaticObject(int x, int y) {
-		super(x, y);
-	}
-
-}
