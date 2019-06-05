4e504f3c19f4501bbfd3ec549510f69fcf118c04
crunchitaip <san.steven97@gmail.com>
2019-06-05 11:40:23 +0200

    poser des blocs

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -27,7 +26,6 @@ import model.game.Map;
 import model.game.Tiles;
 
 public class GameController extends Controller {
-
 
 	private GestionCollision detecteur;
 
@@ -73,11 +71,9 @@ public class GameController extends Controller {
 		charapane.getChildren().add(bill.getImage());
 		floor.getChildren().addAll(mv.creerVue());
 		addListen();
-		
 
 	}
-	
-	
+
 	public void addListen() {
 		viewAbleSol.addListener(new ListChangeListener<Tiles>() {
 
@@ -120,10 +116,9 @@ public class GameController extends Controller {
 							floor.getChildren().removeIf(img -> img.getLayoutY() > 33 * 32);
 							break;
 						case "mouse":
-							
+
 							break;
 						}
-						
 
 					}
 				}
@@ -136,56 +131,46 @@ public class GameController extends Controller {
 			@Override
 			public void onChanged(Change<? extends Node> c) {
 				// TODO Auto-generated method stub
-				while(c.next()) {
+				while (c.next()) {
 					if (c.wasRemoved()) {
-						if(delete == "mouse") {
-							for( Node t : c.getRemoved()) {
-								if(t instanceof TileView) {
+						if (delete == "mouse") {
+							for (Node t : c.getRemoved()) {
+								if (t instanceof TileView) {
 									ObservableList<Tiles> ListSol = mapPrincipale.getTilesListSol();
-									ObservableList<Tiles> ListMid = mapPrincipale.getTilesListMid();
+									
 									int[][] tabSol = mapPrincipale.getMapSol();
-									int[][] tabMid = mapPrincipale.getMapMid();
- 									TileView tView = (TileView)t;
- 									Tiles tile = tView.getTile();
+						
+									TileView tView = (TileView) t;
+									Tiles tile = tView.getTile();
 									ListSol.remove(tile);
 									viewAbleSol.remove(tile);
-									tabSol[tile.getX()][tile.getY()]=0;
-									/*if(tabMid[tile.getX()][tile.getY()]!=0) {
-										for(Tiles tileMid : ListMid) {
-											if (tileMid.getX() == tile.getX() && tileMid.getY() == tile.getY()) {
-												TileView tv = new TileView(tileMid);
-												tv.relocate(tileMid.getX()*32, tileMid.getY() * 32);
-												floor.getChildren().add(tv);
-											}
-										}
-										
-										
-									}*/
+									tabSol[tile.getX()][tile.getY()] = 0;
 									
+
 								}
 							}
 						}
 					}
-					if(c.wasAdded()) {
-						if(add == "mouse") {
-							for( Node t : c.getAddedSubList()) {
-								if(t instanceof TileView) {
+					if (c.wasAdded()) {
+						if (add == "mouse") {
+							for (Node t : c.getAddedSubList()) {
+								if (t instanceof TileView) {
 									ObservableList<Tiles> ListSol = mapPrincipale.getTilesListSol();
- 									TileView tView = (TileView)t;
- 									Tiles tile = tView.getTile();
+									TileView tView = (TileView) t;
+									Tiles tile = tView.getTile();
 									ListSol.add(tile);
-									//viewAbleSol.add(tile);
-									System.out.println(tile.getX()+"  "+tile.getY());
-									mapPrincipale.setTileSol(tile.getX(), tile.getY(), 1);
+									// viewAbleSol.add(tile);
+									mapPrincipale.setTileSol(tile.getX(), tile.getY(), tile.getCode());
 								}
 							}
 						}
 					}
 				}
 			}
-			
+
 		});
 	}
+
 	public void isAlive() {
 
 		bill.setLife(heart);
@@ -332,7 +317,7 @@ public class GameController extends Controller {
 			if (countY < 0) {
 				bill.getChrac().move("Up");
 				addImages("Up");
-				 deleteImages("Down");
+				deleteImages("Down");
 				countY += 32;
 			}
 			break;
@@ -357,29 +342,23 @@ public class GameController extends Controller {
 	public void addImages(String direction) {
 		ObservableList<Tiles> ListSol = mapPrincipale.getTilesListSol();
 		ObservableList<Tiles> ListMid = mapPrincipale.getTilesListMid();
-		/*int [][] mapMid = mapPrincipale.getMapMid();
-		int [][] mapSol = mapPrincipale.getMapSol();*/
+		/*
+		 * int [][] mapMid = mapPrincipale.getMapMid(); int [][] mapSol =
+		 * mapPrincipale.getMapSol();
+		 */
 		switch (direction) {
-		
 
 		case "Right":
 			add = "Right";
-			
-			
-			
-			
-			/*	for(int y = addLignTop; y <= addLignBot; y++) {
-					if(mapMid[(addLignX + 61) % 300][y]!=0) {
-						Tiles t = new Tiles ((addLignX + 61) % 300,y,mapMid[(addLignX + 61) % 300][y]);
-						viewAbleSol.add(t);
-						}
-					if(mapSol[(addLignX + 61) % 300][y]!=0) {
-					Tiles t = new Tiles ((addLignX + 61) % 300,y,mapSol[(addLignX + 61) % 300][y]);
-					viewAbleSol.add(t);
-					}
-				}*/
-				
-			
+
+			/*
+			 * for(int y = addLignTop; y <= addLignBot; y++) { if(mapMid[(addLignX + 61) %
+			 * 300][y]!=0) { Tiles t = new Tiles ((addLignX + 61) % 300,y,mapMid[(addLignX +
+			 * 61) % 300][y]); viewAbleSol.add(t); } if(mapSol[(addLignX + 61) % 300][y]!=0)
+			 * { Tiles t = new Tiles ((addLignX + 61) % 300,y,mapSol[(addLignX + 61) %
+			 * 300][y]); viewAbleSol.add(t); } }
+			 */
+
 			for (Tiles tile : ListMid)
 				if (tile.getX() == (addLignX + 61) % 300 && addLignTop <= tile.getY() && tile.getY() <= addLignBot)
 					viewAbleSol.add(tile);
@@ -417,17 +396,13 @@ public class GameController extends Controller {
 				if (tile.getX() < 60 && tile.getY() == addLignBot) {
 					viewAbleSol.add(tile);
 				}
-			/*for(int x = 0; x < 60; x++) {
-				if(mapMid[x][addLignBot]!=0) {
-					Tiles t = new Tiles (x,addLignBot,mapMid[x][addLignBot]);
-					viewAbleSol.add(t);
-					}
-				if(mapSol[x][addLignBot]!=0) {
-					Tiles t = new Tiles (x,addLignBot,mapSol[x][addLignBot]);
-					viewAbleSol.add(t);
-					}
-			}*/
-			
+			/*
+			 * for(int x = 0; x < 60; x++) { if(mapMid[x][addLignBot]!=0) { Tiles t = new
+			 * Tiles (x,addLignBot,mapMid[x][addLignBot]); viewAbleSol.add(t); }
+			 * if(mapSol[x][addLignBot]!=0) { Tiles t = new Tiles
+			 * (x,addLignBot,mapSol[x][addLignBot]); viewAbleSol.add(t); } }
+			 */
+
 			addLignBot++;
 
 			break;
@@ -542,34 +517,61 @@ public class GameController extends Controller {
 	public void removeKeyCode(KeyCode k) {
 		keyPressed.remove(k);
 	}
-	public void test(javafx.scene.input.MouseEvent k) {
-		delete="mouse";
-		add="mouse";
-		if(k.isPrimaryButtonDown())
-			floor.getChildren().removeIf(img -> img.getLayoutY() >= k.getY()-32 && img.getLayoutY() < k.getY() && img.getLayoutX() >= k.getX()-32 && img.getLayoutX() < k.getX());
-		if(k.isSecondaryButtonDown()) {
-			Tiles t = new Tiles ((int)k.getX()/32,(int)k.getY()/32,1);
-			TileView tv = new TileView(t);
-			tv.relocate(t.getX()*32, t.getY() * 32-4);
-			floor.getChildren().add(tv);
-			}
-		/*case "mouse":
-								ObservableList<Tiles> ListSol = mapPrincipale.getTilesListSol();
-								int[][] tabSol = mapPrincipale.getMapSol();
-								ListSol.remove(tile);
-								tabSol[tile.getX()][tile.getY()]=0;
-								System.out.println(tile.getX() + "  "+ tile.getY());
-								floor.getChildren().removeIf(img -> (int)img.getLayoutX()/32== tile.getX() && (int)img.getLayoutY()/32 == tile.getY()-1);
-								break;
-								public void test(javafx.scene.input.MouseEvent k) {
-									delete="mouse";
-									System.out.println(viewAbleSol.size());
-								//	System.out.println((int)k.getX()/32+"  "+(int)k.getY()/32);
-									if(k.isPrimaryButtonDown())
-										viewAbleSol.removeIf(img -> img.getY() == (int)k.getY()/32);
-									System.out.println(viewAbleSol.size());
 
-							}*/
+	public void test(javafx.scene.input.MouseEvent k) {
+//		System.out.println(bill.getChrac().getX() - 30 + (int) k.getX() / 32 + "  "
+//				+ (bill.getChrac().getY() - 16 + (int) k.getY() / 32));
+		delete = "mouse";
+		
+		int coordX = bill.getChrac().getX() - 30 + (int) k.getX() / 32;
+		int coordY = bill.getChrac().getY() - 16 + (int) k.getY() / 32;
+		if (k.isPrimaryButtonDown()) {
+			add="background";
+			floor.getChildren().removeIf(img -> img.getLayoutY() >= k.getY() - 32 && img.getLayoutY() < k.getY()
+					&& img.getLayoutX() >= k.getX() - 32 && img.getLayoutX() < k.getX());
+			ObservableList<Tiles> ListMid = mapPrincipale.getTilesListMid();
+			int[][] tabMid = mapPrincipale.getMapMid();
+			if(tabMid[coordX][coordY]!=0 && tabMid[coordX][coordY]!=6 && tabMid[coordX][coordY]!=7 && tabMid[coordX][coordY]!=8 ) {
+				 for(Tiles tileMid : ListMid) {
+					 	if (tileMid.getX() == coordX && tileMid.getY() == coordY) {
+					 		TileView tv = new TileView(tileMid);
+					 		tv.relocate(((int) k.getX() / 32) * 32 - 32 + countX, ((int) k.getY() / 32) * 32-countY);
+					 		floor.getChildren().add(tv);
+					 	}
+				 }
+			 }
+		}
+		if (k.isSecondaryButtonDown()) {
+			add = "mouse";
+			int[][] tabSol = mapPrincipale.getMapSol();
+			
+			if(coordX < 0) coordX+=300;
+			else if (coordX >= 300) coordX -= 300;
+			if (tabSol[coordX][coordY] == 0) {
+				Tiles t = new Tiles(coordX, coordY, 2);
+				TileView tv = new TileView(t);
+				tv.relocate(((int) k.getX() / 32) * 32 - 32 + countX, ((int) k.getY() / 32) * 32-countY);
+				floor.getChildren().add(tv);
+			}
+		}
+		// case "mouse":
+		// ObservableList<Tiles> ListSol = mapPrincipale.getTilesListSol();
+		// int[][] tabSol = mapPrincipale.getMapSol();
+		// ListSol.remove(tile);
+		// tabSol[tile.getX()][tile.getY()]=0;
+		// System.out.println(tile.getX() + " "+ tile.getY());
+		// floor.getChildren().removeIf(img -> (int)img.getLayoutX()/32== tile.getX() &&
+		// (int)img.getLayoutY()/32 == tile.getY()-1);
+		// break;
+		// public void test(javafx.scene.input.MouseEvent k) {
+		// delete="mouse";
+		// System.out.println(viewAbleSol.size());
+		// // System.out.println((int)k.getX()/32+" "+(int)k.getY()/32);
+		// if(k.isPrimaryButtonDown())
+		// viewAbleSol.removeIf(img -> img.getY() == (int)k.getY()/32);
+		// System.out.println(viewAbleSol.size());
+		//
+		// }
 	}
 
 }
